77e4662dae6602ffafd3bc41fa826d0f57720f4f
T <56058912+tenchorussev@users.noreply.github.com>
2024-04-09 23:33:35 +0300

    Initial commit

---
+++ app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { LoaderComponent } from './shared/loader/loader.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+   
+    LoaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
